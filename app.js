1772c6c4895e0949b4c5b4183ae48a5dbdf34c43
franco427 <calatayfranco719@gmail.com>
2023-08-02 15:49:55 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const publicPath = path.resolve(__dirname,'./public');
+
+const PUERTO=3000
+
+app.use(express.static(publicPath));
+
+app.listen(PUERTO,()=>{
+console.log("run port "+PUERTO)
+})
+
+app.get('/home',(req,res)=>{
+res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.post('/register',(req,res)=>{
+    res.redirect("/home")
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+
+app.post('/login',(req,res)=>{
+    res.redirect("/home")
+})
